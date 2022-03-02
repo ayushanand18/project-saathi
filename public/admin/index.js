599dc62640ccac4bb9cd499e0d5c8e04501404fc
Ayush Anand <aa.21u10425@btech.nitdgp.ac.in>
2022-03-02 12:46:40 +0000

    FEAT: Updated DB Link
    DESC: Successfully integrated database with the chatbot. It will also collect information of what user typed that resulted in a fallback, note no personal data including the IP is collected.

---
+++ public/admin/index.js
@@ -0,0 +1,7 @@
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+    apiKey: "AIzaSyDmqqFTQARiYAmLJ7jLGlACCpYzzkQSR5o",
+    authDomain: "project-saathi-43be5.firebaseapp.com",
+    projectId: "project-saathi-43be5"
+  });
+var db = firebase.firestore();
